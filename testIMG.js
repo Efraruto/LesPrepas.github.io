4d38fc9ae2ff8175106b733686799d3c1bc513c2
Efrayim <efrayim.abou@gmail.com>
2026-03-25 18:03:44 +0100

    Update multiple HTML pages and remove unused JavaScript test files.

--- testIMG.js
+++
@@ -1,6 +0,0 @@
-async function test() {
-    const r = await fetch('https://image.pollinations.ai/prompt/cat');
-    console.log('Status:', r.status);
-    console.log('Headers:', r.headers.get('content-type'));
-}
-test();
